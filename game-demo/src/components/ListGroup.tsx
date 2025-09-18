9ddb515404baa32405202dbdf633afc2facd0db5
Brian_KIp <yobra8504@gmail.com>
2025-09-19 00:30:08 +0300

    conditional Rendering

--- game-demo/src/components/ListGroup.tsx
+++ game-demo/src/components/ListGroup.tsx
@@ -1,9 +1,14 @@
 function ListGroup() {
-  const towns = ["Nairobi", "York", "Paris", "London"];
+  let towns = ["Nairobi", "York", "Paris", "London"];
+  towns = [];
 
   return (
     <>
       <h1>List</h1>
+     {/* {towns.length ===0 ? <p>No item found</p>:null} */}
+      
+        {towns.length === 0 && <p>No Item Found</p>}
+      
       <ul className="list-group">
         {towns.map((town) => (
           <li key={town}>{town}</li>
